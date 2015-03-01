3b2bc24bf50b2e8ef46a0a13a9435e305a159a71
Samuel GRAU <samuel.grau@gmail.com>
2015-03-01 13:33:49 +0100

    Initial Commit

---
+++ SFErrors/SFErrors.h
@@ -0,0 +1,19 @@
+//
+//  SFErrors.h
+//  SFErrors
+//
+//  Created by Samuel Grau on 01/03/2015.
+//  Copyright (c) 2015 Samuel Grau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFErrors.
+FOUNDATION_EXPORT double SFErrorsVersionNumber;
+
+//! Project version string for SFErrors.
+FOUNDATION_EXPORT const unsigned char SFErrorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFErrors/PublicHeader.h>
+
+
